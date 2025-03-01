0aa3048d5254c8e59bdb82f14cff9c8538f7ba09
RAVISH111 <ravishmaurya.kumar@gmail.com>
2025-03-01 22:03:20 +0530

    Updated code

--- colaboration-app/src/App.jsx
+++ colaboration-app/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       userimg: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>`,
       username:"john here",
       content: newPost,
-      img: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>`,
+      img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWIdG-fxuqhGWDspjk63js4TNZPkatZ5Ulmw&s`,
       likes: 232,
       comment: [],
     };
@@ -85,19 +85,19 @@ function App() {
       </div>
       <div className="posts">
         {posts.map((post) => (
-          <div key={post.id} className="post" style={{ backgroundColor: "white", padding: "15px", marginBottom: "15px", borderRadius: "8px" }}>
+          <div key={post.id} className="post" >
             <div className="post-header">
               <span dangerouslySetInnerHTML={{ __html: post.userimg }}></span>
               <span>{post.username}</span>
               <div className="options">...</div>
             </div>
             <hr />
-            <p style={{color:"black"}}>{post.content}</p>
+            <p>{post.content}</p>
             {post.img && <img src={post.img} alt="Post" className="post-image" />}
             <hr />
             <div className="actions">
               <button onClick={() => likePost(post.id, post.likes)}>Like ({post.likes})</button>
-              <button onClick={() => setCommentBox(commentBox === post.id ? null : post.id)}>Comment</button>
+              <button onClick={() => setCommentBox(commentBox === post.id ? null : post.id)}>Comment ({post.comment?.length || 0})</button>
             </div>
             {commentBox === post.id && (
               <div className="comment-section" style={{ marginTop: "10px" }}>
